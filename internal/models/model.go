4bae67ecac55275dd5ea52bfd32f7029bc652c15
Baldomero Cho <don.baldo@hotmail.com>
2024-03-18 04:58:05 -0600

    remove struct Ride, supporte interface{}

--- internal/models/model.go
+++ internal/models/model.go
@@ -1,31 +1,7 @@
 package models
 
-import "time"
-
 type Record struct {
-	ID     string `json:"id"`
-	Data   Ride   `json:"data"`
-	Action string `json:"action"`
-}
-
-type Ride struct {
-	PassengerId string `json:"passenger_id"`
-	Origin      struct {
-		Long int `json:"long"`
-		Lat  int `json:"lat"`
-	} `json:"origin"`
-	Destination struct {
-		Long int `json:"long"`
-		Lat  int `json:"lat"`
-	} `json:"destination"`
-	DepartureTime  time.Time `json:"departure_time"`
-	ArrivalTime    time.Time `json:"arrival_time"`
-	Fare           float64   `json:"fare"`
-	Distance       float64   `json:"distance"`
-	Duration       float64   `json:"duration"`
-	VehicleType    string    `json:"vehicle_type"`
-	RecomendedFare float64   `json:"recomended_fare"`
-	Id             string    `json:"id"`
-	UpdatedAt      time.Time `json:"updated_at"`
-	CreatedAt      time.Time `json:"created_at"`
+	ID     string      `json:"id"`
+	Data   interface{} `json:"data"`
+	Action string      `json:"action"`
 }
